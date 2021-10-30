58fb2950b73c5e53b9aaf18b6184aa018e19b4d7
marlo <marlo@DESKTOP-H63Q5P1>
2021-10-29 19:51:52 -0500

    Agregar archivos de proyecto.

---
+++ repasos1/repasos1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace repasos1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            String usuario = txtUsuario.Text;
+            String constrasena = txtContrasena.Text;
+
+            String usuariou = "estudiante2021";
+            String contrasenau = "uisrael2021";
+
+
+            if (usuario != usuariou)
+            {
+                DisplayAlert("Mensaje de Alerta", "Usuario o Contraseña Incorrecta", "OK");
+            }
+            if (constrasena != contrasenau)
+            {
+                DisplayAlert("Mensaje de Alerta", "Usuario o Contraseña Incorrecta", "OK");
+            }
+            else if (usuario == usuariou && constrasena == contrasenau)
+            {
+                await Navigation.PushAsync(new Registro(usuario, constrasena));
+
+            }
+
+
+        }
+
+    }
+}
+}
+ 
---
+++ repasos1/repasos1/Registro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace repasos1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
